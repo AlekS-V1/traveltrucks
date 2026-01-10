1adcad5170f7e060045bacb49d8fddfd54890e3b
Oleksandr Vakulin <vlipoq@gmail.com>
2026-01-10 02:44:44 +0100

    add style filter menu

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -3,6 +3,7 @@
 import CamperList from "@/components/CamperList/CamperList";
 import { getCampers, Camper } from "@/lib/api";
 import { useState } from "react";
+import css from "./catalog.module.css";
 
 const Catalog = () => {
   const [campers, setCampers] = useState<Camper[]>([]);
@@ -13,10 +14,91 @@ const Catalog = () => {
       }
     }
   return (
-    <section>
-        <h1>Catalog Page</h1>
-        <button onClick={handlClick}>Load Campers</button>
-        {campers?.length > 0 &&<CamperList catalog={campers}/>}
+    <section className={css.catalogSection}>
+
+        <div className={css.container}>
+          <div className={css.catalogMenu}>
+            <div>
+              <h6 className={css.label}>Location</h6>
+              <div className={css.inputWrapper}>
+                <input className={css.input} name="location" type="text" placeholder="Enter location"/>
+                <svg className={css.iconInput} width="20" height="20">
+                  <use href='/sprite.svg#map' />
+                </svg>
+              </div>
+            </div>
+            <div className={css.filterContainer}>
+              <h5 className={css.titleFiltrs}>Filtr</h5>
+
+              <div className={css.equipmentContainer}>
+
+                <h6 className={css.labelFiltr}>Vehicle equipment</h6>
+              <div className={css.borderFiltr} />
+              <div className={css.filterButtons}>
+                <div className={css.buttonFilter}>
+                  <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#ac' />
+                  </svg>
+                  <p>AC</p>
+                </div>
+              <div className={css.buttonFilter}>
+                <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#diagram' />
+                  </svg>
+                  <p>Automatic</p>
+                  </div>
+              <div className={css.buttonFilter}>
+                <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#cup' />
+                  </svg>
+                  <p>Kitchen</p>
+                  </div>
+              <div className={css.buttonFilter}>
+                <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#tv' />
+                  </svg>
+                  <p>TV</p>
+                  </div>
+              <div className={css.buttonFilter}>
+                <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#bathroom' />
+                  </svg>
+                  <p>Bathroom</p>
+                  </div>
+              </div>
+
+              </div>
+              
+            <div className={css.vehicleTypeContainer}>
+
+              <h6  className={css.labelFiltr}>Vehicle type</h6>
+              <div className={css.borderFiltr} />
+              <div className={css.filterButtons}>
+                <button className={css.buttonFilter}>
+                  <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#van' />
+                  </svg>Van</button>
+                <button className={css.buttonFilter}>
+                  <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#full_integrated' />
+                  </svg>Fully Integrated</button>
+                <button className={css.buttonFilter}>
+                  <svg className={css.iconFilter} width="32" height="32">
+                    <use href='/sprite.svg#alcove' />
+                  </svg>Alcove</button>
+              </div>
+
+            </div>
+              
+            </div>
+            <button className={css.buttonSearch} onClick={handlClick}>Search</button>
+          </div>
+
+          <div className={css.catalog}>
+            {campers?.length > 0 &&<CamperList catalog={campers}/>}
+          </div>
+        </div>
+
     </section>
   ); 
 };
--- components/CamperList/CamperList.tsx
+++ components/CamperList/CamperList.tsx
@@ -1,12 +1,13 @@
 import { Camper } from "@/lib/api"
 import CamperItem from "../CamperItem/CamperItem";
+import css from "./CamperList.module.css";
 
 type Props = {
     catalog: Camper[];
 }
 const CamperList = ({catalog}: Props) => {
     return (
-        <ul>
+        <ul className={css.camperList}>
             {catalog.map((camper) => (
                 <CamperItem key={camper.id} item={camper}/>
             ))}
