80d17ae3192e08f4363b6fc0f7ae5a8db121ec6a
Oleksandr Vakulin <vlipoq@gmail.com>
2026-01-16 13:18:04 +0100

    created form, add formik, yuap

--- components/Book/Book.tsx
+++ components/Book/Book.tsx
@@ -1,11 +1,109 @@
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik";
 import css from "./Book.module.css";
+import * as Yup from "yup";
+
+
+interface BookingFormValues {
+    username: string;
+    email: string;
+    booking_date: string;
+    comment: string;
+}
+ const initialValues: BookingFormValues = {
+    username: "",
+    email: "",
+    booking_date: "",
+    comment: "",
+ }
+
+ const BookFormSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Name must be at least 2 characters")
+    .max(30, "Name is too long")
+    .required("Name is required"),
+  email: Yup.string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  booking_date: Yup.string()
+    .required("Date is required"),
+  comment: Yup.string(),
+});
+
 
 const Book = () => {
+    const handleSubmit = (
+        values: BookingFormValues,
+        actions: FormikHelpers<BookingFormValues>,
+    ) => {
+        console.log("Booking:", values );
+        actions.resetForm();
+    }
     return (
-        <form className={css.book} action="submit">
-            <h3>Book your campervan now</h3>
-            <p>Stay connected! We are always ready to help you.</p>
-        </form>
+        <Formik 
+            initialValues={initialValues} 
+            onSubmit={handleSubmit}
+            validationSchema={BookFormSchema}>
+            <Form className={css.book}>
+                <fieldset className={css.fieldset}>
+                    <Field 
+                        className={css.input} 
+                        type="text" 
+                        name="username"
+                        placeholder="Name*" 
+                    />
+                    <ErrorMessage 
+                        name="username" 
+                        component="span" 
+                        className={css.error} 
+                    />
+                </fieldset>
+                <fieldset className={css.fieldset}>
+                    <Field 
+                        type="text" 
+                        name="email" 
+                        className={css.input}
+                        placeholder="Email*" 
+                    />
+                    <ErrorMessage 
+                        name="email" 
+                        component="span" 
+                        className={css.error} 
+                    />
+                </fieldset>
+                <fieldset className={css.fieldset}>
+                    <Field 
+                        type="text" 
+                        name="booking_date" 
+                        className={css.input}
+                        placeholder="Booking date*" 
+                    />
+                    <ErrorMessage 
+                        name="booking_date" 
+                        component="span" 
+                        className={css.error} 
+                    />
+                </fieldset>
+                <fieldset className={css.fieldset}>
+                    <Field 
+                        as="textarea" 
+                        name="comment" 
+                        rows={4} 
+                        className={css.inputarea}
+                        placeholder="Comment" 
+                    />
+                    <ErrorMessage 
+                        name="comment" 
+                        component="span" 
+                        className={css.error} 
+                    />
+                </fieldset>
+                <button 
+                    type="submit" 
+                    className={css.button}
+                    >Send
+                </button>
+            </Form>
+        </Formik>
     )
 }
 export default Book;
